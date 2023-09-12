abd1133adde9d009838d0ef6c42e23828324657c
kboleiro <143357132+rafaelcarlosoficial@users.noreply.github.com>
2023-09-12 17:22:01 -0300

    login-screen.js pronto
    Login-screen 100% concluído
    CSS ok
    HTML ok
    JS ok

---
+++ js/login-screen.js
@@ -0,0 +1,18 @@
+ const onePlayer = document.querySelector('[data-one-player]');
+ const twoPlayers = document.querySelector('[data-two-players]');
+ 
+ onePlayer.addEventListener('click', () => {
+ window.location.href = '../html/one-player.html';
+ 
+ });
+ 
+ twoPlayers.addEventListener('click', () => {
+    window.location.href = '../html/two-players.html';
+ });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
