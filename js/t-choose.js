31c0a4bef93afa69613546c0aa2b0035e3533b0e
kboleiro <143357132+rafaelcarlosoficial@users.noreply.github.com>
2023-09-17 18:09:28 -0300

    Tela 1 funcionando, tela 2 bugada

--- js/t-choose.js
+++ js/t-choose.js
@@ -1,14 +1,16 @@
 const animals = document.querySelectorAll("[data-animal]");
-const avatar = document.querySelector(".avatar");
+const avatar = document.querySelector("[data-avatar]");
+const avatarTwo = document.querySelector("[data-avatar-two]");
 const btnConfirm = document.querySelectorAll("[btn-confirm]");
 const btnConfirmTwo = document.querySelectorAll("[btn-confirm-two]");
 const btnContinue = document.querySelector("[btn-continue]");
 const btnContinueTwo = document.querySelectorAll('[btn-continue-two]');
+const scriptTag = document.querySelector("script[data-screen]");
+const nameScreen = scriptTag.getAttribute("data-screen");
 const initialWidth = window.innerWidth;
 let widthScreen;
-console.log(btnContinue);
 
-const changeAvatar = (name) => {
+const changeAvatar = (name, avatar) => {
   switch (name) {
     case "Seahorse":
       avatar.setAttribute("src", "../images/Seahorse-avatar.png");
@@ -41,24 +43,54 @@ const changeAvatar = (name) => {
   }
 };
 
+const changeAnimalTwo = (element) => {
+  const animal = element.target;
+  const name = animal.getAttribute("alt");
+  const image = animal.getAttribute("src");
+
+  const profileName = document.querySelector("[data-name-two]");
+  const profileImage = document.querySelector("[data-image-two]");
+
+  profileImage.setAttribute("src", image);
+  profileName.innerHTML = name;
+  
+  if (avatarTwo.style.transform.includes("translateX(60vw)")) {
+    setTimeout(() => {
+      avatarTwo.style.transform = "translateX(0)";
+      changeAvatar(name, avatarTwo);
+    }, 100);
+  } else {
+    setTimeout(() => {
+      avatarTwo.style.transform = "translateX(60vw)";
+    }, 100);
+
+    setTimeout(() => {
+      changeAvatar(name, avatarTwo);
+    }, 350);
+
+    setTimeout(() => {
+      avatarTwo.style.transform = "translateX(0)";
+    }, 700);
+  }
+
+};
+  
+
 const changeAnimal = (element) => {
   const animal = element.target;
   const name = animal.getAttribute("alt");
   const image = animal.getAttribute("src");
-  console.log(image);
 
   const profileName = document.querySelector("[data-name]");
   const profileImage = document.querySelector("[data-image]");
 
   profileImage.setAttribute("src", image);
   profileName.innerHTML = name;
-
-  const avatar = document.querySelector(".avatar");
-
+  
   if (avatar.style.transform.includes("translateX(-60vw)")) {
     setTimeout(() => {
       avatar.style.transform = "translateX(0)";
-      changeAvatar(name);
+      changeAvatar(name, avatar);
     }, 100);
   } else {
     setTimeout(() => {
@@ -66,20 +98,21 @@ const changeAnimal = (element) => {
     }, 100);
 
     setTimeout(() => {
-      changeAvatar(name);
+      changeAvatar(name, avatar);
     }, 350);
 
     setTimeout(() => {
       avatar.style.transform = "translateX(0)";
     }, 700);
   }
+
 };
+
+
 
 const confirmTheChoice = (click) => {
   const confirm = click.target;
-  console.log(confirm.textContent);
   if (confirm.textContent === "Edit") {
-    
     confirm.style.backgroundColor = "var(--orange)";
     confirm.textContent = "Confirm";
     btnContinue.style.display = "none";
@@ -90,16 +123,11 @@ const confirmTheChoice = (click) => {
   } else {
     confirm.style.backgroundColor = "var(--light-orange)";
     confirm.textContent = "Edit";
-    
-    if (btnContinue === undefined) { 
-      btnContinueTwo.style.display = "block"
-    } else {
-      console.log("entrou no 96")
-      btnContinue.style.display = "block";
-    }
     animals.forEach((animal) => {
       animal.removeEventListener("click", animalClick);
     });
+    confirmBothPlayers(nameScreen);
+    
   }
 };
 
@@ -110,6 +138,20 @@ const changeSaturation = (element) => {
   
 }
 
+const removeSaturation = (array) => {
+  array.forEach(element => {
+  if (element.classList.contains('saturation-card')) { 
+      element.classList.remove('saturation-card');
+    }
+  });
+}
+
+const releaseContinueButton = (event) => {
+  console.log("entrou")
+  btnContinue.style.display = 'block';
+};
+
+
 const animalClick = (event) => {
   removeSaturation(animals)
   changeAnimal(event)
@@ -117,15 +159,28 @@ const animalClick = (event) => {
 }
 
 
-const removeSaturation = (array) => {
-  array.forEach(element => {
-  console.log('122', element)
-  if (element.classList.contains('saturation-card')) { 
-      console.log("123", removeSaturation)
-      element.classList.remove('saturation-card');
-    }
-   
-  });
+const animalClickTwo = (event) => {
+  removeSaturation(animals)
+  changeAnimalTwo(event)
+  changeSaturation(event)
+  releaseContinueButton(event)
+}
+
+
+const confirmBothPlayers = (nameScreen) => {
+
+  if(nameScreen === 'two-player'){
+    btnConfirmTwo.forEach((btn) => {
+      btn.classList.remove('display-btn');
+    });
+    btnContinue.style.display = "none";
+    animals.forEach((animal) => {
+      animal.addEventListener('click', animalClickTwo);
+    });
+    
+  } else { 
+  btnContinue.style.display = "block";
+  }
 }
 
 animals.forEach((animal) => {
@@ -149,7 +204,6 @@ btnContinueTwo.forEach((btn) => {
     window.location.href = '../html/game-2-play.html';
   })
 });
-
 
 
 btnContinue.addEventListener("click", () => {
